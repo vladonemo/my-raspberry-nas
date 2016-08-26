f94c7fa7faf1dbcd2bc15d16f3aaea1e24895324
vladonemo <vlado.nemo@gmail.com>
2016-08-26 09:45:59 +0200

    Using hdparm.conf instead of calling hdparm in rc.local

---
+++ home/pi/showDriveStates.sh
@@ -0,0 +1,8 @@
+echo "Media drive state"
+hdparm -C /dev/disk/by-uuid/5C080F18080EF0B6
+echo ""
+echo "Backup drive state"
+hdparm -C /dev/disk/by-uuid/5A22DB2D22DB0CBF
+echo ""
+echo "Main drive state"
+hdparm -C /dev/disk/by-uuid/8620238620237C75

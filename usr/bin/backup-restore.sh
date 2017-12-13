bd41e66353f1e73106b057dc366b55f4620d52f9
Vladimir Nemergut <vlado.nemo@gmail.com>
2017-12-13 19:45:41 +0100

    Fixing the backup-restore script - adding the extract path and swapping the order of unpacking in increments

--- usr/bin/backup-restore.sh
+++ usr/bin/backup-restore.sh
@@ -1,7 +1,9 @@
 ARCHIVE_TO_RESTORE=$1
+EXTRACT_DIR=$2
 RESTORE_DIR=/media/USBHDD_Backup/Backup/$ARCHIVE_TO_RESTORE
-EXTRACT='tar --incremental -xvp'
+EXTRACT='tar --incremental -xvp -C '$2
 
-cat $RESTORE_DIR/*full.tar | $EXTRACT
+echo Extracting $ARCHIVE_TO_RESTORE using the following arguments: $EXTRACT
+cat $RESTORE_DIR/*full.tar* | $EXTRACT
 
-for i in `ls -r $RESTORE_DIR/*incremental.tar`; do cat $i | $EXTRACT; done;
+for i in `ls $RESTORE_DIR/*incremental.tar*`; do cat $i | $EXTRACT; done;
